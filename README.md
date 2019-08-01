0cd6eee073f3e8768da4a464af447adc296ccd3b
webberg <webbery@foxmail.com>
2019-08-01 14:26:36 +0800

    增加新浪新闻数据集导出脚本

---
+++ README.md
@@ -0,0 +1,10 @@
+#### WikiExtractor.py
+使用链接：https://github.com/attardi/wikiextractor
+#### THUCNewsExtractor.py
+##### 数据集：THUCNews
+链接：https://pan.baidu.com/s/13C2s4eencWdUH6MmLIbfiw 
+提取码：aqkg  
+使用方式：  
+THUCNewsExtractor.py  
+[input dir] direcotory of unzip files  
+[-o] [output dir] extract directory
---
+++ THUCNewsExtractor.py
@@ -0,0 +1,76 @@
+#导出数据到单一的文件中，并切分为训练集、测试集
+import os
+import sys
+import argparse
+import math
+import random
+
+def main():
+    parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]),
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description=__doc__)
+    parser.add_argument("input", help="where is THUCNews")
+    parser.add_argument("-o","--output",
+        help="directory for generate train set and test set")
+
+    args = parser.parse_args()
+
+    rootdir = args.input
+    outdir = args.output
+    
+    if not os.path.exists(rootdir):
+        print(rootdir," not exist")
+        return
+
+    directories = os.listdir(rootdir)
+    last_char = rootdir[len(rootdir)-1]
+    if last_char!='/' or last_char!='\\':
+        rootdir += '/'
+
+    outdir = os.path.abspath(outdir)+'/'
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    #按类别读取文件，将每个文件的内容输出为一行，然后70%作为训练集，30%作为测试集，保存到指定目录
+    for clazz in directories:
+        clazz_dir = rootdir+clazz
+        files = os.listdir(clazz_dir)
+        sentences=[]
+        file_num = 0
+        print('start ',clazz)
+        for file_name in files:
+            df = open(clazz_dir+'/'+file_name,'r',encoding='utf-8')
+            sentence=""
+            for line in df.readlines():
+                sentence +=line
+
+            sentences.append(sentence)
+            df.close()
+            if file_num%1000==0:
+                print(file_name)
+            file_num+=1
+            # if file_num>1000:
+            #     break
+        
+        total_count_of_class = len(sentences)
+        ## create test set
+        output_dir = outdir+clazz
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        test_set = open(output_dir+'/test_set','a',encoding='utf-8')
+        for times in range(int(total_count_of_class*0.3)):
+            index = random.randint(0,total_count_of_class-times-1)
+            test_set.write(sentences[index])
+            # print(sentences[index],len(sentences))
+            sentences.pop(index)
+        test_set.close()
+
+        train_set=open(output_dir+'/train_set','a',encoding='utf-8')
+        for sentence in sentences:
+            train_set.write(sentence)
+        train_set.close()
+
+        print('finish ',clazz)
+
+if __name__ == '__main__':
+    main()
