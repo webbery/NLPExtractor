a26dd617a1baf5f1fd315257e7c097876966b26f
webberg <webbery@foxmail.com>
2019-08-02 18:05:15 +0800

    使用多线程读取文件

--- THUCNewsExtractor.py
+++ THUCNewsExtractor.py
@@ -4,6 +4,46 @@ import sys
 import argparse
 import math
 import random
+import json
+from threading import Thread
+
+def extract_text(clazz_dir,files):
+    sentences=[]
+    file_num = 0
+    print('start ',clazz)
+    for file_name in files:
+        df = open(clazz_dir+'/'+file_name,'r',encoding='utf-8')
+        sentence=""
+        for line in df.readlines():
+            sentence +=line
+
+        sentences.append(json.dumps({"text":sentence},ensure_ascii=False))
+        df.close()
+        if file_num%1000==0:
+            print(file_name)
+        file_num+=1
+        # if file_num>1000:
+        #     break
+    
+    total_count_of_class = len(sentences)
+    ## create test set
+    output_dir = outdir+clazz
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    test_set = open(output_dir+'/test_set','a',encoding='utf-8')
+    for times in range(int(total_count_of_class*0.3)):
+        index = random.randint(0,total_count_of_class-times-1)
+        test_set.write(sentences[index]+'\n')
+        # print(sentences[index],len(sentences))
+        sentences.pop(index)
+    test_set.close()
+
+    train_set=open(output_dir+'/train_set','a',encoding='utf-8')
+    for sentence in sentences:
+        train_set.write(sentence+'\n')
+    train_set.close()
+
+    print('finish ',clazz)
 
 def main():
     parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]),
@@ -35,42 +75,9 @@ def main():
     for clazz in directories:
         clazz_dir = rootdir+clazz
         files = os.listdir(clazz_dir)
-        sentences=[]
-        file_num = 0
-        print('start ',clazz)
-        for file_name in files:
-            df = open(clazz_dir+'/'+file_name,'r',encoding='utf-8')
-            sentence=""
-            for line in df.readlines():
-                sentence +=line
-
-            sentences.append(sentence)
-            df.close()
-            if file_num%1000==0:
-                print(file_name)
-            file_num+=1
-            # if file_num>1000:
-            #     break
+        thread = Thread(target=extract_text,args=(clazz_dir,files))
+        thread.start()
+        # extract_text(clazz_dir,files)
         
-        total_count_of_class = len(sentences)
-        ## create test set
-        output_dir = outdir+clazz
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-        test_set = open(output_dir+'/test_set','a',encoding='utf-8')
-        for times in range(int(total_count_of_class*0.3)):
-            index = random.randint(0,total_count_of_class-times-1)
-            test_set.write(sentences[index])
-            # print(sentences[index],len(sentences))
-            sentences.pop(index)
-        test_set.close()
-
-        train_set=open(output_dir+'/train_set','a',encoding='utf-8')
-        for sentence in sentences:
-            train_set.write(sentence)
-        train_set.close()
-
-        print('finish ',clazz)
-
 if __name__ == '__main__':
     main()
